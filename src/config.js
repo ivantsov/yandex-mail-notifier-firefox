0a10adfb5bb5bfe36b89526decc4d55a00a1c611
Alexander Ivantsov <alexivantsov@ya.ru>
2015-10-11 23:08:29 +0500

    reafactoring config

---
+++ src/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    id: 'yandex-mail-notifier',
+    icon: './icon.png',
+    iconGray: './icon-gray.png',
+    openURL: 'https://mail.yandex.ru/',
+    parseURL: 'https://mail.yandex.ru/lite/inbox'
+};
--- src/lib/config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    id: 'yandex-mail-notifier',
-    label: 'Перейти на Яндекс.Почта',
-    cssFile: 'style.css',
-    urlForParse: 'https://mail.yandex.ru/lite/inbox',
-    urlForOpen: 'https://mail.yandex.ru/'
-};
-
-module.exports = config;
