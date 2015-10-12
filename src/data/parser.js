bffd903cea8a453d7d8b1ab5d3b5a169bceb9d6e
Alexander Ivantsov <alexivantsov@ya.ru>
2015-10-12 23:45:29 +0500

    remove old files

--- src/data/parser.js
+++
@@ -1,8 +0,0 @@
-var selector = document.querySelector('.b-folders__folder_current .b-folders__folder__num'),
-    unreadMsgCount = 0;
-
-if (selector) {
-    unreadMsgCount = parseInt(selector.innerHTML, 10);
-}
-
-self.postMessage(unreadMsgCount);
