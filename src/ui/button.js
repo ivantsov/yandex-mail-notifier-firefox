c162372532c78eb457ce3ec74e6848a9719a2fde
Alexander Ivantsov <alexivantsov@ya.ru>
2015-10-24 17:43:27 +0500

    update button

--- src/ui/button.js
+++ src/ui/button.js
@@ -1,26 +1,25 @@
-var _ = require('sdk/l10n').get;
-var {ActionButton} = require('sdk/ui/button/action');
-var {id, icon} = require('../config');
-var {openTab} = require('./tab');
+const _ = require('sdk/l10n').get;
+const {prefs} = require('sdk/simple-prefs');
+const {ActionButton} = require('sdk/ui/button/action');
+const {ID, ICON, ICON_GRAY} = require('../config');
+const {addHandler, getState} = require('../observer');
+const {openTab} = require('../services/tab');
+const {showPanel} = require('./panel');
 
-// TODO: use ES6 class when it will be released
-
-var button;
-
-function init() {
-    button = ActionButton({
-        id,
-        icon,
+module.exports.init = function () {
+    const button = ActionButton({
+        id: ID,
+        icon: ICON_GRAY,
         label: _('buttonLabel'),
-        onClick: openTab
+        onClick() {
+            prefs.showPanel && getState().isAuth ? showPanel(button) : openTab()
+        }
     });
-}
 
-function setState(newState) {
-    button.state(button, newState);
-}
-
-module.exports = {
-    init,
-    setState
+    addHandler(({isAuth, unreadCount}) => {
+        button.state(button, {
+            badge: unreadCount || null,
+            icon: isAuth ? ICON : ICON_GRAY
+        });
+    });
 };
