5bf964e5024da9cdb1af2cefc2ee6d3da0e35f74
Alexander Ivantsov <alexivantsov@ya.ru>
2015-10-24 17:43:14 +0500

    update config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,25 @@
+const DOMAIN = 'https://mail.yandex.ru';
+const API_URL = `${DOMAIN}/api/`;
+
 module.exports = {
-    id: 'yandex-mail-notifier',
-    icon: './icon.png',
-    iconGray: './icon-gray.png',
-    openURL: 'https://mail.yandex.ru/',
-    parseURL: 'https://mail.yandex.ru/lite/inbox'
+    ID: 'yandex-mail-notifier',
+    ICON: './icon.png',
+    ICON_GRAY: './icon-gray.png',
+    DOMAIN,
+    API_URL,
+    UNREAD_MESSAGE_URL: `${API_URL}/v2/bar/counters?silent`,
+    IGNORED_FOLDERS: ['spam', 'archive', 'trash', 'sent', 'outbox', 'draft'],
+    RECONNECT_INTERVAL: 60 * 1000, // 1 min
+    SOCKET: {
+        CREDENTIALS_URL: `${DOMAIN}/neo2/handlers/xiva_sub.jsx`,
+        URL: 'wss://xiva-daria.mail.yandex.net/events/websocket/',
+        RECONNECT_INTERVAL: 30 * 60 * 1000 //30 min
+    },
+    COOKIE: {
+        HOST: '.yandex.ru',
+        SESSION_ID: 'Session_id',
+        UID: 'yandexuid',
+        PATH: '/',
+        TIMEOUT: 1000
+    }
 };
