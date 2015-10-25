ca7e7a0213d378b6a600825f9dc4e3ab6a78baee
Alexander Ivantsov <alexivantsov@ya.ru>
2015-10-26 00:39:16 +0500

    update panel components

--- src/panel/components/item.js
+++ src/panel/components/item.js
@@ -1,28 +1,33 @@
 import React, {PropTypes} from 'react';
+const {DOMAIN} = require('../../config');
+const l10n = require('../utils/l10n');
 
 const Item = React.createClass({
     propTypes: {
-		from: PropTypes.string.isRequired,
-		firstline: PropTypes.string.isRequired,
-		date: PropTypes.string.isRequired,
-        dispatch: PropTypes.func.isRequired,
-		subject: PropTypes.string
+        id: PropTypes.string.isRequired,
+        from: PropTypes.string.isRequired,
+        subject: PropTypes.string.isRequired,
+        firstline: PropTypes.string.isRequired,
+        date: PropTypes.object.isRequired
     },
     render() {
-		const {
-			from,
-			firstline,
-			date,
-			subject
-		} = this.props;
+        const {
+            id,
+            from,
+            subject,
+            firstline,
+            date,
+        } = this.props;
 
         return (
-            <a className="email" href="#">
+            <a className="email" href={`${DOMAIN}/#message/${id}`}>
                 <p className="email__header">
                     <span className="email__title">{from}</span>
-                    <span className="email__date">{date}</span>
+                    <span className="email__date">{l10n.date(date)}</span>
                 </p>
+
                 <p className="email__subject">{subject}</p>
+
                 <p className="email__content">{firstline}</p>
             </a>
         );
--- src/panel/components/list.js
+++ src/panel/components/list.js
@@ -3,7 +3,9 @@ const Item = require('./item');
 
 const List = React.createClass({
     propTypes: {
-        items: PropTypes.arrayOf(PropTypes.object).isRequired
+        items: PropTypes.arrayOf(PropTypes.shape({
+            id: PropTypes.string.isRequired
+        })).isRequired
     },
     render() {
         const items = this.props.items.map(item => <Item key={item.id} {...item}/>);
