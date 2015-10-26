80ff21f22975257bcec1178b797bce17d6c54a7a
Alexander Ivantsov <alexivantsov@ya.ru>
2015-10-26 23:39:57 +0500

    refactoring panel store

--- src/panel/store.js
+++ src/panel/store.js
@@ -4,6 +4,4 @@ const rootReducer = require('./reducers');
 
 const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
 
-module.exports = function configureStore(initialState) {
-    return createStoreWithMiddleware(rootReducer, initialState);
-};
+module.exports = createStoreWithMiddleware(rootReducer);
