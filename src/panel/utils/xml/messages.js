fcbb15116a516ebf6713f7a5ba069444d941a91f
Alexander Ivantsov <alexivantsov@ya.ru>
2015-10-26 23:38:46 +0500

    refactoring xml messages parser

--- src/panel/utils/xml/messages.js
+++ src/panel/utils/xml/messages.js
@@ -1,41 +1,69 @@
 const xml = require('./parser');
+const {IGNORED_FOLDERS} = require('../../../config');
 
-function parseMessage(message) {
-    let subject = xml.getText(xml.select('subject', message), 'text');
+function getFilteredMessage(messages, folders) {
+    return messages.filter(({fid}) => {
+        const folder = folders.find(({id}) => id === fid);
 
-    if (subject === 'No subject') {
-        subject = '';
-    }
+        return (!folder || IGNORED_FOLDERS.indexOf(folder.symbol) < 0);
+    });
+}
 
-    return {
-        subject,
-        id: xml.getAttr(message, 'mid'),
-        from: xml.getText(xml.select('from', message), 'name') || xml.getText(xml.select('from', message), 'email'),
-        firstline: xml.getText(message, 'firstline'),
-        date: new Date(xml.getAttr(message, 'recv_date')),
-        attach: parseInt(xml.getAttr(message, 'att_count'), 10),
-        fid: xml.getAttr(message, 'fid')
-    };
+function parseFolders(folders) {
+    return [...xml.selectAll('folder', folders)].map(folder => {
+        return {
+            id: xml.getText(folder, 'fid'),
+            symbol: xml.getText(folder, 'symbol'),
+            name: xml.getText(folder, 'name').split('|').pop()
+        };
+    });
+}
+
+function parseMessages(messages) {
+    return [...xml.selectAll('message', messages)].map(message => {
+        let subject = xml.getText(xml.select('subject', message), 'text');
+
+        if (subject === 'No subject') {
+            subject = '';
+        }
+
+        return {
+            subject,
+            id: xml.getAttr(message, 'mid'),
+            from: xml.getText(xml.select('from', message), 'name') || xml.getText(xml.select('from', message), 'email'),
+            firstline: xml.getText(message, 'firstline'),
+            date: new Date(xml.getAttr(message, 'recv_date')),
+            attach: parseInt(xml.getAttr(message, 'att_count'), 10),
+            fid: xml.getAttr(message, 'fid')
+        };
+    });
 }
 
 function parse(xmlDoc) {
-    const list = xml.select('mailbox_list', xmlDoc);
+    let messages = xml.select('mailbox_list', xmlDoc);
+    let folders = xml.select('folder_list', xmlDoc);
 
-    if (!list) {
+    if (!messages || !folders) {
         throw new Error('Bad response format in messages xml');
     }
 
-    const error = xml.select('error', list);
+    const error = xml.select('error', messages);
 
     if (error) {
         throw new Error(`Error occurred while parsing messages xml ${xml.getAttr(error, 'code')}`);
     }
 
-    const details = xml.select('details', list);
+    const details = xml.select('details', messages);
+    const unreadCount = parseInt(xml.getAttr(details, 'msg_count'), 10);
+
+    messages = parseMessages(messages);
+    folders = parseFolders(folders);
+
+    messages = getFilteredMessage(messages, folders);
 
     return {
-        unreadCount: parseInt(xml.getAttr(details, 'msg_count'), 10),
-        messages: [...xml.selectAll('message', list)].map(parseMessage)
+        unreadCount,
+        messages
     };
 }
 
