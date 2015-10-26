cbb3c9ad74599ab10a330d9cf61c8767cc424c19
Alexander Ivantsov <alexivantsov@ya.ru>
2015-10-26 22:41:13 +0500

    add i10n module for panel

---
+++ src/panel/utils/l10n.js
@@ -0,0 +1,35 @@
+function text(key) {
+    return document.getElementById(key).textContent;
+}
+
+function addNil(value) {
+    if (value < 10) {
+        value = `0${value}`;
+    }
+
+    return value;
+}
+
+function date(value) {
+    const currentDate = new Date();
+    let yearPostfix = '';
+
+    if (currentDate.toDateString() === value.toDateString()) {
+        const hour = addNil(value.getHours());
+        const min = addNil(value.getMinutes());
+
+        return `${hour}:${min}`;
+    }
+    else if (currentDate.getYear() !== value.getYear()) {
+        yearPostfix = ` ${value.getFullYear()}`;
+    }
+
+    const month = text('month' + (value.getMonth() + 1));
+
+    return `${value.getDate()} ${month}${yearPostfix}`;
+}
+
+module.exports = {
+    text,
+    date
+};
