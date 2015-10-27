49db1a51a5aa2ceed654f6e69afbe35fd7981104
Alexander Ivantsov <alexivantsov@ya.ru>
2015-10-27 09:16:16 +0500

    add webpack config

---
+++ webpack.config.dev.js
@@ -0,0 +1,17 @@
+module.exports = {
+    entry: './src/panel/index.js',
+    output: {
+        path: './data/panel',
+        filename: './index.js'
+    },
+    resolve: {
+        extensions: ['', '.js']
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel',
+            exclude: /node_modules/
+        }]
+    }
+};
