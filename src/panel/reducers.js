8e97198f636baeef85833651ae34799e9f6ac74e
Alexander Ivantsov <alexivantsov@ya.ru>
2015-10-27 09:16:03 +0500

    update panel reducer

--- src/panel/reducers.js
+++ src/panel/reducers.js
@@ -1,24 +1,28 @@
 const {
-    LOAD_MESSAGES,
-    LOAD_MESSAGES_SUCCESS,
-    LOAD_MESSAGES_ERROR
+    LOAD_DATA,
+    LOAD_DATA_SUCCESS,
+    LOAD_DATA_ERROR
 } = require('./constants');
 
 const initialState = {
-    loading: true,
+    loading: false,
+    user: '',
+    unreadCount: 0,
     messages: []
 };
 
 function reducer(state = initialState, action) {
     switch (action.type) {
-        case LOAD_MESSAGES:
+        case LOAD_DATA:
             return {
                 ...state,
                 loading: true
             };
-        case LOAD_MESSAGES_SUCCESS:
+        case LOAD_DATA_SUCCESS:
             return {
                 loading: false,
+                user: action.user,
+                unreadCount: action.unreadCount,
                 messages: action.messages
             };
         default:
