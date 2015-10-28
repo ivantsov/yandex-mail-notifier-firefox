1ea31c6eb5f50c4d3dfe6b2cdcd60428a79b79d4
Alexander Ivantsov <alexander.ivantsov@graph.uk>
2015-10-28 11:36:28 +0500

    refactoring api module with using XHR, add getUserInfo method

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-const {Request} = require('sdk/request');
-const {UNREAD_MESSAGE_URL, SOCKET} = require('../config');
+const {XMLHttpRequest} = require('sdk/net/xhr');
+const {API_URL, UNREAD_MESSAGE_URL, SOCKET} = require('../config');
 
 function createError(res) {
     let err = new Error(res.statusText);
@@ -18,28 +18,61 @@ function createUnauthorizedError(res) {
     });
 }
 
-function request(url) {
+function request({url, type}) {
     return new Promise((resolve, reject) => {
-        Request({
-            url,
-            onComplete(res) {
-                if (res.status >= 200 && res.status < 300) {
-                    if (res.json.code === 'AUTH_NO_AUTH') {
+        const xhr = new XMLHttpRequest();
+
+        xhr.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                let result;
+
+                if (type === 'json') {
+                    result = JSON.parse(this.response);
+
+                    if (result.code === 'AUTH_NO_AUTH') {
                         reject(createError(createUnauthorizedError(res)));
                     }
+                }
+                else if (type === 'xml') {
+                    result = this.responseXML;
+                }
 
-                    resolve(res.json)
-                }
-                else {
-                    reject(createError(res));
-                }
+                resolve(result);
             }
-        }).get();
+            else {
+                reject(createError(this));
+            }
+        };
+
+        xhr.onerror = function () {
+            reject(createError(this));
+        };
+
+        xhr.open('GET', url, true);
+        xhr.send();
+    });
+}
+
+function getUserInfo() {
+    return request({
+        url: `${API_URL}settings_setup`,
+        type: 'xml'
+    }).then(res => {
+        const error = res.querySelector('error');
+
+        if (error) {
+            throw new Error(`Error occurred while parsing user xml ${error.getAttribute('code')}`);
+        }
+
+        return res.querySelector('body').querySelector('default_email').textContent;
     });
 }
 
 function getUnreadCount() {
-    return request(UNREAD_MESSAGE_URL).then(res => res.counters.unread);
+    return request({
+        url: UNREAD_MESSAGE_URL,
+        type: 'json'
+    }).then(res => res.counters.unread);
 }
 
 function getSocketCredentials(uid) {
@@ -47,10 +80,14 @@ function getSocketCredentials(uid) {
         return Promise.reject(createError(createUnauthorizedError()));
     }
 
-    return request(`${SOCKET.CREDENTIALS_URL}?req=${uid}`);
+    return request({
+        url: `${SOCKET.CREDENTIALS_URL}?req=${uid}`,
+        type: 'json'
+    });
 }
 
 module.exports = {
+    getUserInfo,
     getUnreadCount,
     getSocketCredentials
 };
