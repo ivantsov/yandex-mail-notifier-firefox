26f74a2e25e5e09da225ee4fff9711f85bea6a48
Alexander Ivantsov <alexander.ivantsov@graph.uk>
2015-10-30 18:54:14 +0500

    add updateMessageStatus action and refactoring loadMessages action

--- src/panel/actions.js
+++ src/panel/actions.js
@@ -1,26 +1,45 @@
 const api = require('./api');
 const {
-    LOAD_MESSAGES,
     LOAD_MESSAGES_SUCCESS,
-    LOAD_MESSAGES_ERROR
+    LOAD_MESSAGES_ERROR,
+
+	UPDATE_MESSAGE_STATUS_SUCCESS,
+	UPDATE_MESSAGE_STATUS_ERROR
 } = require('./constants');
 
 function loadMessages() {
     return dispatch => {
-        dispatch({type: LOAD_MESSAGES});
-
         return api.loadMessages()
             .then(data =>
                 dispatch({
                     type: LOAD_MESSAGES_SUCCESS,
-                    data
+					data
                 })
-            )
-            .catch(err => console.error(err.stack));
+            ).catch(err => {
+				dispatch({type: LOAD_MESSAGES_ERROR});
+
+				console.error(err.message, err.stack);
+			});
     };
 }
 
+function updateMessageStatus(data) {
+	return dispatch => {
+		return api.updateMessageStatus(data)
+			.then(() => {
+				dispatch({
+					type: UPDATE_MESSAGE_STATUS_SUCCESS,
+					id: data.id
+				})
+			}).catch(err => {
+				dispatch({type: UPDATE_MESSAGE_STATUS_ERROR});
+
+				console.error(err.message, err.stack);
+			});
+	};
+}
 
 module.exports = {
-    loadMessages
+    loadMessages,
+	updateMessageStatus
 };
