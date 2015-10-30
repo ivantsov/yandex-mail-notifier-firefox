65535289805b857d2667c01258ee82057cb14af7
Alexander Ivantsov <alexivantsov@ya.ru>
2015-10-30 21:45:16 +0500

    fix indention

--- src/services/cookie.js
+++ src/services/cookie.js
@@ -7,24 +7,24 @@ const {newURI} = Cc['@mozilla.org/network/io-service;1'].getService(nsIIOService
 const {DOMAIN, COOKIE: {SESSION_ID, HOST, PATH}} = require('../config');
 
 function getCookie(key) {
-	const uri = newURI(DOMAIN, null, null);
-	const cookiesString = getCookieString(uri, null);
-	const cookiePairs = querystring.parse(cookiesString, '; ', '=');
+    const uri = newURI(DOMAIN, null, null);
+    const cookiesString = getCookieString(uri, null);
+    const cookiePairs = querystring.parse(cookiesString, '; ', '=');
 
-	return cookiePairs[key];
+    return cookiePairs[key];
 }
 
 function addChangeListener(handler) {
-	events.on('cookie-changed', ({data, subject}) => {
-		const {name, host, path} = subject.QueryInterface(nsICookie2);
+    events.on('cookie-changed', ({data, subject}) => {
+        const {name, host, path} = subject.QueryInterface(nsICookie2);
 
-		if (name === SESSION_ID && host === HOST && path === PATH) {
-			handler(data === 'added');
-		}
-	}, true);
+        if (name === SESSION_ID && host === HOST && path === PATH) {
+            handler(data === 'added');
+        }
+    }, true);
 }
 
 module.exports = {
-	getCookie,
-	addChangeListener
+    getCookie,
+    addChangeListener
 };
