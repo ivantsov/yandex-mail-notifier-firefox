363f6ec0a8ca88339c90d5f5db4883b6c65b41a8
Alexander Ivantsov <alexander.ivantsov@graph.uk>
2015-10-30 15:06:12 +0500

    extract creating xhr object to separate module, add updateMessagesStatus api method

--- src/panel/api.js
+++ src/panel/api.js
@@ -1,41 +1,28 @@
-const {API_URL, IGNORED_FOLDERS} = require('../config');
+const {get, post} = require('./utils/request');
 const parser = require('./utils/parser');
 
-function logError(status, res) {
-    var error = new Error(status);
-
-    error.response = res;
-
-    console.error(error);
+function loadMessages() {
+    return get('mailbox_list?first=0&last=100&extra_cond=only_new&goto=all').then(parser);
 }
 
-function request(url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
+function updateMessageStatus({oper, id}) {
+    return post({
+        url: 'mailbox_oper',
+        params: {
+            ids: [id],
+            oper
+        }
+    }).then(res => {
+        const errorTag = res.querySelector('error');
+        const error = errorTag ? errorTag[0] : null;
 
-        xhr.onload = function() {
-            if (this.status >= 200 && this.status < 300) {
-                resolve(this.responseXML);
-            } else {
-                logError(this.statusText, this.response);
-                reject();
-            }
-        };
-
-        xhr.onerror = function () {
-            logError(this.statusText, this.response);
-            reject();
-        };
-
-        xhr.open('GET', url, true);
-        xhr.send();
+        if (error) {
+            throw new Error(error);
+        }
     });
 }
 
-function loadMessages() {
-    return request(`${API_URL}mailbox_list?first=0&last=100&extra_cond=only_new&goto=all`).then(parser);
-}
-
 module.exports = {
-    loadMessages
+    loadMessages,
+    updateMessageStatus
 };
