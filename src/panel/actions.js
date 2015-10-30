4a88b12f778996cb968f35c236119cd467202eb1
Alexander Ivantsov <alexivantsov@ya.ru>
2015-10-30 22:13:25 +0500

    cleanup panel redux components

--- src/panel/actions.js
+++ src/panel/actions.js
@@ -2,9 +2,8 @@ const api = require('./api');
 const {
     LOAD_MESSAGES_SUCCESS,
     LOAD_MESSAGES_ERROR,
-
-	UPDATE_MESSAGE_STATUS_SUCCESS,
-	UPDATE_MESSAGE_STATUS_ERROR
+    UPDATE_MESSAGE_STATUS_SUCCESS,
+    UPDATE_MESSAGE_STATUS_ERROR
 } = require('./constants');
 
 function loadMessages() {
@@ -13,33 +12,35 @@ function loadMessages() {
             .then(data =>
                 dispatch({
                     type: LOAD_MESSAGES_SUCCESS,
-					data
+                    data
                 })
-            ).catch(err => {
-				dispatch({type: LOAD_MESSAGES_ERROR});
+            )
+            .catch(err => {
+                dispatch({type: LOAD_MESSAGES_ERROR});
 
-				console.error(err.message, err.stack);
-			});
+                console.error(err.message, err.stack); // eslint-disable-line no-console
+            });
     };
 }
 
 function updateMessageStatus(data) {
-	return dispatch => {
-		return api.updateMessageStatus(data)
-			.then(() => {
-				dispatch({
-					type: UPDATE_MESSAGE_STATUS_SUCCESS,
-					id: data.id
-				})
-			}).catch(err => {
-				dispatch({type: UPDATE_MESSAGE_STATUS_ERROR});
+    return dispatch => {
+        return api.updateMessageStatus(data)
+            .then(() => {
+                dispatch({
+                    type: UPDATE_MESSAGE_STATUS_SUCCESS,
+                    id: data.id
+                });
+            })
+            .catch(err => {
+                dispatch({type: UPDATE_MESSAGE_STATUS_ERROR});
 
-				console.error(err.message, err.stack);
-			});
-	};
+                console.error(err.message, err.stack); // eslint-disable-line no-console
+            });
+    };
 }
 
 module.exports = {
     loadMessages,
-	updateMessageStatus
+    updateMessageStatus
 };
--- src/panel/store.js
+++ src/panel/store.js
@@ -2,8 +2,8 @@ const {createStore, applyMiddleware} = require('redux');
 const thunkMiddleware = require('redux-thunk');
 const reducers = require('./reducers');
 
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
+function createStoreWithMiddleware(initialState) {
+    return applyMiddleware(thunkMiddleware)(createStore)(reducers, initialState);
+}
 
-module.exports = function (initialState) {
-    return createStoreWithMiddleware(reducers, initialState);
-};
+module.exports = createStoreWithMiddleware;
