03bf98d2ff80917d5bd163bf12a273cf0cbf9270
Alexander Ivantsov <alexivantsov@ya.ru>
2015-11-02 09:19:03 +0500

    refactoring socket service

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,49 +1,76 @@
 const {setTimeout} = require('sdk/timers');
 const {Page} = require('sdk/page-worker');
-const {RECONNECT_INTERVAL, SOCKET, COOKIE: {UID}} = require('../config');
-const {getUserInfo, getUnreadCount, getSocketCredentials} = require('./api');
-const {getCookie} = require('./cookie');
-const {updateState} = require('../observer');
+const {Class} = require('sdk/core/heritage');
+const {RECONNECT_INTERVAL, SOCKET, COOKIE} = require('../config');
+const {getCookie} = require('../utils/cookie');
+const {getUserInfo, getUnreadCount, getSocketCredentials} = require('../utils/api');
+const observer = require('../observer');
 
-const worker = Page({
-    contentURL: './blank.html',
-    contentScriptFile: './scripts/socket.js'
-});
-const emit = worker.port.emit;
-
-function connect() {
-    const uid = getCookie(UID);
-
-    Promise.all([
-        getUserInfo(),
-        getUnreadCount(),
-        getSocketCredentials(uid)
-    ]).then(([user, unreadCount, credentials]) => {
-        setTimeout(reconnect, SOCKET.RECONNECT_INTERVAL); // eslint-disable-line no-use-before-define
-
-        emit('connect', credentials);
-
-        updateState({
-            user,
-            unreadCount
+const SocketService = Class({
+    initialize() {
+        this.worker = Page({
+            contentURL: './blank.html',
+            contentScriptFile: './scripts/socket.js'
         });
-    }).catch(() => setTimeout(connect, RECONNECT_INTERVAL));
-}
 
-function disconnect() {
-    emit('disconnect');
-}
+        this.addListeners();
+    },
+    addListeners() {
+        // UID might not be set immediately - run timeout
+        observer.addListener('login', () => setTimeout(() => this.connect(), COOKIE.TIMEOUT));
+        observer.addListener('logout', () => this.disconnect());
 
-function reconnect() {
-    disconnect();
-    connect();
-}
+        this.worker.port.on('reconnect', this.reconnect);
 
-worker.port.on('reconnect', reconnect);
-worker.port.on('newMessage', message => updateState({unreadCount: message.new_messages}));
-worker.port.on('changeStatus', () => getUnreadCount().then(unreadCount => updateState({unreadCount})));
+        this.worker.port.on('updateUnreadCount', ({operation, new_messages: unreadCount, mid: id, hdr_from: from, hdr_subject: subject, firstline}) => {
+            if (operation === 'insert') {
+                const nameMatch = from.match(/^"(.+)"/);
+                const emailMatch = from.match(/<(.+)>$/);
 
-module.exports = {
-    connect,
-    disconnect
-};
+                observer.emitEvent('newMessage', {
+                    unreadCount,
+                    newMessage: {
+                        id,
+                        from: nameMatch[1] || emailMatch[1],
+                        subject: subject !== 'No subject' ? subject : '',
+                        firstline
+                    }
+                });
+            }
+            else if (unreadCount !== undefined) {
+                observer.emitEvent('unreadCountChanged', {unreadCount});
+            }
+            else {
+                getUnreadCount().then(unreadCount => observer.emitEvent('unreadCountChanged', {unreadCount}));
+            }
+        });
+    },
+    connect() {
+        const uid = getCookie(COOKIE.UID);
+
+        Promise.all([
+            getUserInfo(),
+            getUnreadCount(),
+            getSocketCredentials(uid)
+        ]).then(([user, unreadCount, credentials]) => {
+            setTimeout(this.reconnect, SOCKET.RECONNECT_INTERVAL); // eslint-disable-line no-use-before-define
+
+            this.worker.port.emit('connect', credentials);
+
+            observer.emitEvent('socket:success', {user, unreadCount});
+        }).catch(() => {
+            setTimeout(this.connect, RECONNECT_INTERVAL);
+
+            observer.emitEvent('socket:error');
+        });
+    },
+    disconnect() {
+        this.worker.port.emit('disconnect');
+    },
+    reconnect() {
+        this.disconnect();
+        this.connect();
+    }
+});
+
+module.exports = new SocketService();
