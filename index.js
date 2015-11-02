e6a165d4efa70e4021f03cbbc5b3cd1a8d30c0a5
Alexander Ivantsov <alexivantsov@ya.ru>
2015-11-02 09:32:10 +0500

    update main entrypoint

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
-const {setTimeout} = require('sdk/timers');
-const {COOKIE: {SESSION_ID, TIMEOUT}} = require('./src/config');
-const {getCookie, addChangeListener} = require('./src/services/cookie');
-const {connect, disconnect} = require('./src/services/socket');
-const {updateStateToInitial} = require('./src/observer');
-const button = require('./src/ui/button');
-
-button.init();
-
-// user is already authenticated
-if (getCookie(SESSION_ID)) {
-    connect();
-}
-
-addChangeListener(isAuth => {
-    if (isAuth) {
-        // user just authorized
-        // UID might not be set immediately - run timeout
-        setTimeout(connect, TIMEOUT);
-    }
-    else {
-        // user left
-        disconnect();
-
-        updateStateToInitial();
-    }
-});
+require('./src/ui/button');
+require('./src/services/notification');
+require('./src/services/socket');
+require('./src/services/cookie'); // should be last
