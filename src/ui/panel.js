42cf131de0b772d241af3cedb10a38658c888f71
Alexander Ivantsov <alexivantsov@ya.ru>
2015-11-02 09:14:08 +0500

    refactoring panel

--- src/ui/panel.js
+++ src/ui/panel.js
@@ -1,66 +1,68 @@
 const _ = require('sdk/l10n').get;
 const {Panel} = require('sdk/panel');
-const {openTab} = require('../services/tab');
-const {getState} = require('../observer');
+const {Class} = require('sdk/core/heritage');
+const {openTab} = require('../utils/tab');
+const observer = require('../observer');
 
-const panel = Panel({
-    width: 700,
-    height: 415,
-    contentURL: './panel/index.html',
-    contentScriptFile: './panel/index.js',
-    contentScriptWhen: 'ready',
-    contentStyleFile: './panel/styles.css',
-    contentScriptOptions: {
-        l10n: {
-            compose: _('compose'),
-            markRead: _('markRead'),
-            spam: _('spam'),
-            remove: _('remove'),
-            month1: _('month1'),
-            month2: _('month2'),
-            month3: _('month3'),
-            month4: _('month4'),
-            month5: _('month5'),
-            month6: _('month6'),
-            month7: _('month7'),
-            month8: _('month8'),
-            month9: _('month9'),
-            month10: _('month10'),
-            month11: _('month11'),
-            month12: _('month12'),
-            emptyList: _('emptyList'),
-            loadingError: _('loadingError'),
-            operationError: _('operationError')
-        }
+const CustomPanel = Class({
+    initialize() {
+        this.panel = Panel({
+            width: 700,
+            height: 415,
+            contentURL: './panel/index.html',
+            contentScriptFile: './panel/index.js',
+            contentScriptWhen: 'ready',
+            contentStyleFile: './panel/styles.css',
+            contentScriptOptions: {
+                l10n: {
+                    compose: _('compose'),
+                    markRead: _('markRead'),
+                    spam: _('spam'),
+                    remove: _('remove'),
+                    month1: _('month1'),
+                    month2: _('month2'),
+                    month3: _('month3'),
+                    month4: _('month4'),
+                    month5: _('month5'),
+                    month6: _('month6'),
+                    month7: _('month7'),
+                    month8: _('month8'),
+                    month9: _('month9'),
+                    month10: _('month10'),
+                    month11: _('month11'),
+                    month12: _('month12'),
+                    emptyList: _('emptyList'),
+                    loadingError: _('loadingError'),
+                    operationError: _('operationError')
+                }
+            },
+            onShow: () => this.onShow(),
+            onHide: () => this.onHide()
+        });
+
+        this.addListeners();
+    },
+    addListeners() {
+        this.panel.port.on('openTab', (url) => {
+            this.panel.hide();
+
+            openTab(url);
+        });
     },
     onShow() {
-        const {user, unreadCount} = getState();
+        const {user, unreadCount} = observer.getState();
 
-        panel.port.emit('show', {
+        this.panel.port.emit('show', {
             user,
             messages: {unreadCount}
         });
     },
     onHide() {
-        panel.port.emit('hide');
+        this.panel.port.emit('hide');
+    },
+    showPanel(position) {
+        this.panel.show({position});
     }
 });
 
-function showPanel(position) {
-    panel.show({position});
-}
-
-function isShown() {
-    return panel.isShowing;
-}
-
-panel.port.on('openTab', (url) => {
-    panel.hide();
-
-    openTab(url);
-});
-
-module.exports = {
-    showPanel,
-    isShown
-};
+module.exports = new CustomPanel();
