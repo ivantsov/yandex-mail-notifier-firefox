ae2d148f7dafe94e4d7dd5f2b8db3f4bb5a29bad
Alexander Ivantsov <alexander.ivantsov@graph.uk>
2015-11-06 19:01:50 +0500

    refactoring notification service (no class)

--- src/services/notification.js
+++ src/services/notification.js
@@ -1,76 +1,91 @@
 const _ = require('sdk/l10n').get;
-const {prefs} = require('sdk/simple-prefs');
+const prefs = require('sdk/simple-prefs');
 const {notify} = require('sdk/notifications');
 const {setInterval, clearInterval} = require('sdk/timers');
-const {Class} = require('sdk/core/heritage');
 const {NOT_AUTH_NOTIFICATION_INTERVAL} = require('../config');
 const observer = require('../observer');
 const {openTab} = require('../utils/tab');
 
-const NotificationService = Class({
-    initialize() {
-        this.notAuthTimer = null;
-        this.unreadTimer = null;
+let unreadTimer = null;
+let notAuthTimer = null;
 
-        this.addListeners();
-    },
-    addListeners() {
-        observer.addListener('login', this.disableNotAuth);
-        observer.addListener('logout', () => {
-            //this.disableUnread();
-            this.enableNotAuth();
-        });
-        observer.addListener('newMessage', this.showNewMessage);
-        //observer.addListener('socket:success', this.enableUnread);
-        //observer.addListener('socket:error', this.disableUnread);
-    },
-    showNotAuth() {
-        notify({
-            title: _('notAuthNotificationTitle'),
-            text: _('notAuthNotificationText'),
-            onClick: openTab
-        });
-    },
-    showUnreadCount() {
-        const unreadCount = observer.getState().unreadCount;
+function showNewMessage({newMessage}) {
+	if (prefs.prefs.newMessageNotification) {
+		const {id, from, subject, firstline} = newMessage;
 
-        if (unreadCount > 0) {
-            notify({
-                title: _('msgNotificationTitle', unreadCount),
-                text: _('msgNotificationText', unreadCount),
-                onClick: openTab
-            });
-        }
-    },
-    showNewMessage({newMessage}) {
-        if (prefs.newMessageNotification) {
-            const {id, from, subject, firstline} = newMessage;
+		notify({
+			title: _('newMessageNotificationTitle', from),
+			text: `${subject}\n${firstline}`,
+			onClick() {
+				openTab(`#message/${id}`);
+			}
+		});
+	}
+}
 
-            notify({
-                title: _('newMessageNotificationTitle', from),
-                text: `${subject}\n${firstline}`,
-                onClick() {
-                    openTab(`#message/${id}`);
-                }
-            });
-        }
-    },
-    enableNotAuth() {
-        this.showNotAuth();
+function enableUnread({user}) {
+	if (prefs.prefs.unreadMessagesNotification && user) {
+		disableUnread();
 
-        this.notAuthTimer = setInterval(this.showNotAuth, NOT_AUTH_NOTIFICATION_INTERVAL);
-    },
-    disableNotAuth() {
-        clearInterval(this.notAuthTimer);
-    },
-    enableUnread() {
-        this.showUnreadCount();
+		unreadTimer = setInterval(() => {
+			const unreadCount = observer.getState().unreadCount;
 
-        this.unreadTimer = setInterval(showUnreadCount, NOT_AUTH_NOTIFICATION_INTERVAL);
-    },
-    disableUnread() {
-        clearInterval(this.unreadTimer);
-    }
-});
+			if (unreadCount > 0) {
+				notify({
+					title: _('unreadMsgNotificationTitle', unreadCount),
+					text: _('unreadMsgNotificationText', unreadCount),
+					onClick: openTab
+				});
+			}
+		}, prefs.prefs.unreadMessagesNotification);
+	}
+}
 
-module.exports = new NotificationService();
+function disableUnread() {
+	clearInterval(unreadTimer);
+}
+
+function enableNotAuth({user}) {
+	if (prefs.prefs.notAuthNotification && user) {
+		disableNotAuth();
+
+		notAuthTimer = setInterval(() => {
+			notify({
+				title: _('notAuthNotificationTitle'),
+				text: _('notAuthNotificationText'),
+				onClick: openTab
+			});
+		}, NOT_AUTH_NOTIFICATION_INTERVAL);
+	}
+}
+
+function disableNotAuth() {
+	clearInterval(notAuthTimer);
+}
+
+function init() {
+	// new message notifications
+	observer.addListener('newMessage', showNewMessage);
+
+	// unread notifications
+	observer.addListener('socket:success', enableUnread);
+	observer.addListener('logout', disableUnread);
+
+	prefs.on('unreadMessagesNotification', () => {
+		const state = observer.getState();
+
+		prefs.prefs.unreadMessagesNotification ? enableUnread(state) : disableUnread(state);
+	});
+
+	// not auth notifications
+	observer.addListener('login', disableNotAuth);
+	observer.addListener('logout', enableNotAuth);
+
+	prefs.on('notAuthNotification', () => {
+		const state = observer.getState();
+
+		prefs.prefs.notAuthNotification ? enableNotAuth(state) : disableNotAuth(state);
+	});
+}
+
+module.exports = init;
