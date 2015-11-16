f77862eb266e244853da15909e8147740e738af2
Alexander Ivantsov <alexivantsov@ya.ru>
2015-11-17 00:10:41 +0500

    rewrite button without class

--- src/ui/button.js
+++ src/ui/button.js
@@ -1,44 +1,37 @@
 const _ = require('sdk/l10n').get;
 const {prefs} = require('sdk/simple-prefs');
 const {ActionButton} = require('sdk/ui/button/action');
-const {Class} = require('sdk/core/heritage');
 const {ID, ICON, ICON_GRAY} = require('../config');
 const {openTab} = require('../utils/tab');
 const observer = require('../observer');
-const panel = require('./panel');
+const {showPanel} = require('./panel');
 
-const Button = Class({
-    initialize() {
-        this.button = ActionButton({
-            id: ID,
-            icon: ICON_GRAY,
-            label: _('buttonLabel'),
-            onClick: () => this.onClick()
-        });
-
-        this.addListeners();
-    },
-    addListeners() {
-        observer.addListener([
-            'logout',
-            'socket:error'
-        ], () => {
-            this.button.badge = null;
-            this.button.icon = ICON_GRAY;
-        });
-
-        observer.addListener([
-            'socket:success',
-            'unreadCountChanged',
-            'newMessage'
-        ], ({unreadCount}) => {
-            this.button.badge = unreadCount || null;
-            this.button.icon = ICON;
-        });
-    },
+const button = ActionButton({
+    id: ID,
+    icon: ICON_GRAY,
+    label: _('buttonLabel'),
     onClick() {
-        prefs.showPanel && observer.getState().user ? panel.showPanel(this.button) : openTab();
+        prefs.showPanel && observer.getState().user ? showPanel(button) : openTab();
     }
 });
 
-module.exports = new Button();
+function init() {
+    observer.addListener([
+        'logout',
+        'socket:error'
+    ], () => {
+        button.badge = null;
+        button.icon = ICON_GRAY;
+    });
+
+    observer.addListener([
+        'socket:success',
+        'unreadCountChanged',
+        'newMessage'
+    ], ({unreadCount}) => {
+        button.badge = unreadCount || null;
+        button.icon = ICON;
+    });
+}
+
+module.exports = init;
