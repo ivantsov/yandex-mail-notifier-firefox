c16cfcb91c163ed98ba6da81edf56ab75f93c139
Alexander Ivantsov <alexivantsov@ya.ru>
2015-11-17 00:12:18 +0500

    rewrite cookie service without class

--- src/services/cookie.js
+++ src/services/cookie.js
@@ -1,37 +1,32 @@
 const events = require('sdk/system/events');
-const {Class} = require('sdk/core/heritage');
 const {Ci: {nsICookie2}} = require('chrome');
 const {COOKIE: {SESSION_ID, HOST, PATH}} = require('../config');
 const {getUnreadCount} = require('../utils/api');
 const observer = require('../observer');
 
-const CookieService = Class({
-    initialize() {
-        this.addListeners();
+function changeAuthStatus() {
+    getUnreadCount()
+        .then(unreadCount => observer.emitEvent('login', {unreadCount}))
+        .catch(() => observer.emitEvent('logout', {user: null}));
+}
 
-        // check current auth status
-        this.changeAuthStatus();
-    },
-    addListeners() {
-        events.on('cookie-changed', ({subject}) => {
-            try {
-                const {name, host, path} = subject.QueryInterface(nsICookie2);
+function init() {
+    events.on('cookie-changed', ({subject}) => {
+        try {
+            const {name, host, path} = subject.QueryInterface(nsICookie2);
 
-                if (name === SESSION_ID && host === HOST && path === PATH) {
-                    this.changeAuthStatus();
-                }
+            if (name === SESSION_ID && host === HOST && path === PATH) {
+                changeAuthStatus();
             }
-            catch (err) {
-                // silent exception
-                return;
-            }
-        }, true);
-    },
-    changeAuthStatus() {
-        getUnreadCount()
-            .then(unreadCount => observer.emitEvent('login', {unreadCount}))
-            .catch(() => observer.emitEvent('logout', {user: null}));
-    }
-});
+        }
+        catch (err) {
+            // silent exception
+            return;
+        }
+    }, true);
 
-module.exports = new CookieService();
+    // check current auth status
+    changeAuthStatus();
+}
+
+module.exports = init;
