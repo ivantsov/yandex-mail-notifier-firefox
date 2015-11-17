da18679a19749ba4ab6ffb80cf323effeb26b4b3
Alexander Ivantsov <alexivantsov@ya.ru>
2015-11-17 09:06:01 +0500

    don't use classes in socket service

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,85 +1,86 @@
-const {setTimeout} = require('sdk/timers');
+const {setTimeout, clearTimeout} = require('sdk/timers');
 const {Page} = require('sdk/page-worker');
-const {Class} = require('sdk/core/heritage');
 const {RECONNECT_INTERVAL, SOCKET, COOKIE} = require('../config');
 const {getCookie} = require('../utils/cookie');
 const {getUserInfo, getUnreadCount, getSocketCredentials} = require('../utils/api');
 const observer = require('../observer');
 
-const SocketService = Class({
-    initialize() {
-        this.worker = Page({
-            contentURL: './blank.html',
-            contentScriptFile: './scripts/socket.js'
-        });
-
-        this.addListeners();
-    },
-    addListeners() {
-        // UID might not be set immediately - run timeout
-        observer.addListener('login', () => setTimeout(this.connect.bind(this), COOKIE.TIMEOUT));
-        observer.addListener('logout', this.disconnect.bind(this));
-
-        this.worker.port.on('reconnect', this.reconnect.bind(this));
-
-        this.worker.port.on('updateUnreadCount', ({
-            operation,
-            new_messages,
-            mid: id,
-            hdr_from: from,
-            hdr_subject: subject,
-            firstline
-        }) => {
-            const unreadCount = parseInt(new_messages, 10);
-
-            if (operation === 'insert') {
-                const nameMatch = from.match(/^"(.+)"/);
-                const emailMatch = from.match(/<(.+)>$/);
-
-                observer.emitEvent('newMessage', {
-                    unreadCount,
-                    newMessage: {
-                        id,
-                        from: nameMatch[1] || emailMatch[1],
-                        subject: subject !== 'No subject' ? subject : '',
-                        firstline
-                    }
-                });
-            }
-            else if (!Number.isNaN(unreadCount)) {
-                observer.emitEvent('unreadCountChanged', {unreadCount});
-            }
-            else {
-                getUnreadCount().then(unreadCount => observer.emitEvent('unreadCountChanged', {unreadCount})); // eslint-disable-line no-shadow
-            }
-        });
-    },
-    connect() {
-        const uid = getCookie(COOKIE.UID);
-
-        Promise.all([
-            getUserInfo(),
-            getUnreadCount(),
-            getSocketCredentials(uid)
-        ]).then(([user, unreadCount, credentials]) => {
-            setTimeout(this.reconnect.bind(this), SOCKET.RECONNECT_INTERVAL); // eslint-disable-line no-use-before-define
-
-            this.worker.port.emit('connect', credentials);
-
-            observer.emitEvent('socket:success', {user, unreadCount});
-        }).catch(() => {
-            setTimeout(this.connect.bind(this), RECONNECT_INTERVAL);
-
-            observer.emitEvent('socket:error', {user: null});
-        });
-    },
-    disconnect() {
-        this.worker.port.emit('disconnect');
-    },
-    reconnect() {
-        this.disconnect();
-        this.connect();
-    }
+const worker = Page({
+    contentURL: './blank.html',
+    contentScriptFile: './scripts/socket.js'
 });
 
-module.exports = new SocketService();
+let timer;
+
+function connect() {
+    const uid = getCookie(COOKIE.UID);
+
+    clearTimeout(timer);
+
+    Promise.all([
+        getUserInfo(),
+        getUnreadCount(),
+        getSocketCredentials(uid)
+    ]).then(([user, unreadCount, credentials]) => {
+        timer = setTimeout(reconnect, SOCKET.RECONNECT_INTERVAL); // eslint-disable-line no-use-before-define
+
+        worker.port.emit('connect', credentials);
+
+        observer.emitEvent('socket:success', {user, unreadCount});
+    }).catch(() => {
+        setTimeout(connect, RECONNECT_INTERVAL);
+
+        observer.emitEvent('socket:error');
+    });
+}
+
+function disconnect() {
+    worker.port.emit('disconnect');
+}
+
+function reconnect() {
+    disconnect();
+    connect();
+}
+
+function init() {
+    // UID might not be set immediately - run timeout
+    observer.addListener('login', () => setTimeout(connect, COOKIE.TIMEOUT));
+    observer.addListener('logout', disconnect);
+
+    worker.port.on('reconnect', reconnect);
+
+    worker.port.on('updateUnreadCount', ({
+        operation,
+        new_messages,
+        mid: id,
+        hdr_from: from,
+        hdr_subject: subject,
+        firstline
+    }) => {
+        const unreadCount = parseInt(new_messages, 10);
+
+        if (operation === 'insert') {
+            const nameMatch = from.match(/^"(.+)"/);
+            const emailMatch = from.match(/<(.+)>$/);
+
+            observer.emitEvent('newMessage', {
+                unreadCount,
+                newMessage: {
+                    id,
+                    from: nameMatch[1] || emailMatch[1],
+                    subject: subject !== 'No subject' ? subject : '',
+                    firstline
+                }
+            });
+        }
+        else if (!Number.isNaN(unreadCount)) {
+            observer.emitEvent('unreadCountChanged', {unreadCount});
+        }
+        else {
+            getUnreadCount().then(unreadCount => observer.emitEvent('unreadCountChanged', {unreadCount})); // eslint-disable-line no-shadow
+        }
+    });
+}
+
+module.exports = init;
