19ea363f6ad7ff9e1da099bbbb48bceb0044e7bf
Alexander Ivantsov <alexivantsov@ya.ru>
2016-04-13 23:48:41 +0500

    fix showing desktop notification while typing a new email or after sending it

--- src/services/socket.js
+++ src/services/socket.js
@@ -55,13 +55,14 @@ function init() {
         operation,
         new_messages,
         mid: id,
+        hdr_status: status,
         hdr_from: from,
         hdr_subject: subject,
         firstline
     }) => {
         const unreadCount = parseInt(new_messages, 10);
 
-        if (operation === 'insert') {
+        if (operation === 'insert' && status === 'New') {
             const nameMatch = from.match(/^"(.+)"/);
             const emailMatch = from.match(/<(.+)>$/);
 
